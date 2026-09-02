64c0066992dc460923d4e5f28f3285c708cd39b8
Gabweb <Gabweb@users.noreply.github.com>
2026-09-02 10:37:19 +0200

    add button to showcase junk-mutations in newer compose

--- app/src/main/java/de/eso/pitestdemo/Greeting.kt
+++ app/src/main/java/de/eso/pitestdemo/Greeting.kt
@@ -1,17 +1,32 @@
 package de.eso.pitestdemo
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier, content: @Composable () -> Unit) {
-    if (name == "") {
-        Text(
-            text = "Hello!",
-            modifier = modifier
-        )
-    } else {
-        content()
+    Box(modifier) {
+        if (name == "") {
+            Text(
+                text = "Hello!",
+                modifier = modifier
+            )
+        } else {
+            content()
+        }
+
+        // Fine
+        Button(onClick = {  }) {
+            Text(text = stringResource(id = R.string.app_name))
+        }
+
+        // Produces junk mutations
+        Button(
+            onClick = {  }
+        ) { Text(text = stringResource(id = R.string.app_name)) }
     }
 }
